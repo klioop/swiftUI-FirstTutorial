3511def2b92ff52feb094fa2a441652bcaa73712
Sam <klioop2@gmail.com>
2021-04-09 22:12:52 +0900

    navigation stack

---
+++ SwiftUIFirstTutorial/SwiftUIFirstTutorial/MyTextView.swift
@@ -0,0 +1,20 @@
+//
+//  MyTextView.swift
+//  SwiftUIFirstTutorial
+//
+//  Created by klioop on 2021/04/09.
+//
+
+import SwiftUI
+
+struct MyTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTextView()
+    }
+}
